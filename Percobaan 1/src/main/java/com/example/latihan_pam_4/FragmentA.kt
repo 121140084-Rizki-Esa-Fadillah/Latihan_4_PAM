57c80e082a1feb576c94e8d3bf4e0da65a2988d1
121140084_Rizki Esa Fadillah <126938786+121140084-Rizki-Esa-Fadillah@users.noreply.github.com>
2024-03-15 22:18:29 +0700

    Add files via upload

---
+++ Percobaan 1/src/main/java/com/example/latihan_pam_4/FragmentA.kt
@@ -0,0 +1,31 @@
+package com.example.latihan_pam_4
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.latihan_pam_4.databinding.FragmentABinding
+
+class FragmentA : Fragment() {
+
+    private lateinit var binding: FragmentABinding // Variabel binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentABinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Button click listener to navigate to FragmentB
+        binding.buttonToFragmentB.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentA_to_fragmentB)
+        }
+    }
+}
---
+++ Percobaan 1/src/main/java/com/example/latihan_pam_4/FragmentB.kt
@@ -0,0 +1,31 @@
+package com.example.latihan_pam_4
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.latihan_pam_4.databinding.FragmentBBinding
+
+class FragmentB : Fragment() {
+
+    private lateinit var binding: FragmentBBinding // Variabel binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Button click listener to navigate back to FragmentA
+        binding.buttonToFragmentA.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentB_to_fragmentA)
+        }
+    }
+}
