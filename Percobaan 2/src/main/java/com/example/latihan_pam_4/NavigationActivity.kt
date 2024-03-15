47c973fb7e79f86b9ddfb5c0fb3b24a4abbd216a
121140084_Rizki Esa Fadillah <126938786+121140084-Rizki-Esa-Fadillah@users.noreply.github.com>
2024-03-16 00:00:11 +0700

    Add files via upload

---
+++ Percobaan 2/src/main/java/com/example/latihan_pam_4/NavigationActivity.kt
@@ -0,0 +1,13 @@
+package com.example.latihan_pam_4
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NavigationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.nav_activity)
+    }
+
+
+}
